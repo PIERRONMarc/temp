7c2a66a771fb4daf2c2d111f76e8dead4617ffd9
PIERRON Marc <pierronmarc@outlook.fr>
2022-04-06 10:47:24 +0200

    make add static

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,12 @@
 class Fraction {
   constructor(public numerator: number, public denominator: number) {}
 
-  add(other: Fraction): void {
-    if (this.denominator !== other.denominator) {
+  static add(f1: Fraction, f2: Fraction): Fraction {
+    if (f1.denominator !== f2.denominator) {
       // Not implemented yet, but that's not the point here
     }
-    this.numerator += other.numerator;
+    const numerator = f1.numerator + f2.numerator
+    return new Fraction(numerator, f1.denominator);
   }
 }
 
@@ -15,9 +16,9 @@ test("Adding fractions in an OOP paradigm", function () {
   const f2 = new Fraction(1, 3);
 
   // WHEN
-  f1.add(f2);
+  const actual = Fraction.add(f1, f2);
 
   // THEN
   const expected = new Fraction(2, 3);
-  expect(f1).toEqual(expected);
+  expect(actual).toEqual(expected);
 });
