9a6b9c7cb79b10e21c5d12de7f6f074c3cd825ed
PIERRON Marc <pierronmarc@outlook.fr>
2022-04-06 11:02:56 +0200

    refactor with function

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,24 +1,28 @@
-class Fraction {
-  constructor(public numerator: number, public denominator: number) {}
+interface Fraction {
+  readonly numerator: number,
+  readonly  denominator: number
+}
 
-  static add(f1: Fraction, f2: Fraction): Fraction {
-    if (f1.denominator !== f2.denominator) {
-      // Not implemented yet, but that's not the point here
-    }
-    const numerator = f1.numerator + f2.numerator
-    return new Fraction(numerator, f1.denominator);
+function add(f1: Fraction, f2: Fraction): Fraction {
+  if (f1.denominator !== f2.denominator) {
+    // Not implemented yet, but that's not the point here
+  }
+
+  return {
+    numerator: f1.numerator + f2.numerator,
+    denominator: f1.denominator
   }
 }
 
 test("Adding fractions in an OOP paradigm", function () {
   // GIVEN
-  const f1 = new Fraction(1, 3);
-  const f2 = new Fraction(1, 3);
+  const f1: Fraction = { numerator: 1, denominator: 3};
+  const f2: Fraction = { numerator: 1, denominator: 3};
 
   // WHEN
-  const actual = Fraction.add(f1, f2);
+  const actual = add(f1, f2);
 
   // THEN
-  const expected = new Fraction(2, 3);
+  const expected: Fraction = { numerator: 2, denominator: 3};
   expect(actual).toEqual(expected);
 });
